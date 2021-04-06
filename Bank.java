18589a2c42e6dbc920fb77c291583d8927cd30e0
Eric Sandoval <esandov8@ucsc.edu>
2021-04-06 13:00:27 -0700

    Add a Bank class that check is a card exists by checking a json file as a database

---
+++ Bank.java
@@ -0,0 +1,51 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Bank {
+  private final static String DB_PATH = "./BankDB.json";
+
+  public static boolean cardExists(String cardNumber) {
+    JSONObject db = readDB();
+
+    if (db == null) {
+      return false;
+    }
+
+    JSONObject[] accounts = (JSONObject[]) db.get("accounts");
+
+    for (JSONObject account : accounts) {
+      if (cardNumber.equals((String) account.get("cardNumber"))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static JSONObject readDB() {
+    FileReader reader;
+    JSONParser parser = new JSONParser();
+    JSONObject db;
+
+    try {
+      reader = new FileReader(DB_PATH);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return null;
+    }
+
+    try {
+      db = (JSONObject) parser.parse(reader);
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
+      return null;
+    }
+
+    return db;
+  }
+}
