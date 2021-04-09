1d9d89812cc66d8dd54af833c6003aab9a736f22
Eric Sandoval <esandov8@ucsc.edu>
2021-04-09 14:13:24 -0700

    Add pin field and getters for Client

--- Client.java
+++ Client.java
@@ -1,12 +1,30 @@
 public class Client {
   private String cardNumber;
+  private String pin;
   private int checkingBalance;
   private int savingsBalance;
 
-  public Client(String cardNumber, int checkingBalance, int savingsBalance) {
+  public Client(String cardNumber, String pin, int checkingBalance, int savingsBalance) {
     this.cardNumber = cardNumber;
+    this.pin = pin;
     this.checkingBalance = checkingBalance;
     this.savingsBalance = savingsBalance;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public String getPin() {
+    return pin;
+  }
+
+  public int getCheckingBalance() {
+    return checkingBalance;
+  }
+
+  public int getSavingsBalance() {
+    return savingsBalance;
   }
 
   public void updateChecking(int quantity) {
