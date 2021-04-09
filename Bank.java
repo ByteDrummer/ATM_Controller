85d0a18703b539660c6e7d3cb0595e30344f4d7d
Eric Sandoval <esandov8@ucsc.edu>
2021-04-08 23:02:57 -0700

    Implement deposit() and move to newer version of json library

--- Bank.java
+++ Bank.java
@@ -1,14 +1,20 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonKey;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
 
 public class Bank {
   private final static String DB_PATH = "./BankDB.json";
+
+  private final static int DEFAULT_BALANCE = Integer.MIN_VALUE;
+  private final static String DEFAULT_CARDNUM = "-1";
 
   // Check the database to see if the card exists
   public static boolean cardExists(String cardNumber) {
@@ -24,12 +30,13 @@ public class Bank {
   // Check the database to see if the pin for a specific card is correct
   public static boolean pinCorrect(String pin, String cardNumber) {
     Object[] result = getClient(cardNumber);
+    JsonObject client;
 
     if (result == null) {
       return false;
     }
 
-    JSONObject client = (JSONObject) result[0];
+    client = (JsonObject) result[0];
 
     if (pin.equals((String) client.get("pin"))) {
       return true;
@@ -42,19 +49,41 @@ public class Bank {
   // database
   public static Object[] getClient(String cardNumber) {
     Object[] result = getClients();
-    JSONArray clients;
+    JsonArray clients;
+    JsonKey cardNumberKey;
 
     if (result == null) {
       return null;
     }
 
-    clients = (JSONArray) getClients()[0];
+
+
+
+
+    JsonObject db = (JsonObject) result[1];
+    System.out.println(Jsoner.prettyPrint(db.toJson()));
+
+
+
+
+
+
+    cardNumberKey = Jsoner.mintJsonKey("cardNumber", DEFAULT_CARDNUM);
+    clients = (JsonArray) getClients()[0];
 
     // Loop through the list of clients and find a matching one
     for (Object clientObj : clients) {
-      JSONObject client = (JSONObject) clientObj;
+      JsonObject client = (JsonObject) clientObj;
 
-      if (cardNumber.equals((String) client.get("cardNumber"))) {
+
+
+      client.put("2", "2");
+      System.out.println(client.toJson());
+      System.out.println(Jsoner.prettyPrint(db.toJson()));
+
+
+
+      if (cardNumber.equals(client.getString(cardNumberKey))) {
         return new Object[] { client, result[1] };
       }
     }
@@ -62,12 +91,11 @@ public class Bank {
     return null;
   }
 
-  // Get a list of clients in the database and retrun reference to the database
+  // Get a list of clients in the database and return reference to the database
   public static Object[] getClients() {
     FileReader reader;
-    JSONParser parser = new JSONParser();
-    JSONObject db;
-    JSONArray clients;
+    JsonObject db;
+    JsonArray clients;
 
     try {
       reader = new FileReader(DB_PATH);
@@ -77,44 +105,89 @@ public class Bank {
     }
 
     try {
-      db = (JSONObject) parser.parse(reader);
-    } catch (IOException | ParseException e) {
+      db = (JsonObject) Jsoner.deserialize(reader);
+    } catch (JsonException e) {
       e.printStackTrace();
       return null;
     }
 
-    clients = (JSONArray) db.get("clients");
+    clients = (JsonArray) db.get("clients");
 
     return new Object[] { clients, db };
   }
 
-  public static long getBalance(String accountName, String cardNumber) {
+  public static int getBalance(String accountName, String cardNumber) {
     Object[] result = getAccount(accountName, cardNumber);
+    JsonObject account;
+    JsonKey balanceKey;
 
     if (result == null) {
-      return  Long.MIN_VALUE;
+      return Integer.MIN_VALUE;
     }
 
-    JSONObject account = (JSONObject) result[0];
+    balanceKey = Jsoner.mintJsonKey("balance", DEFAULT_BALANCE);
+    account = (JsonObject) result[0];
 
-    return (long) account.get("balance");
+    return account.getInteger(balanceKey);
   }
 
   public static Object[] getAccount(String accountName, String cardNumber) {
     Object[] result = getClient(cardNumber);
+    JsonObject client;
+    JsonObject account;
 
     if (result == null) {
-      return  null;
+      return null;
     }
 
-    JSONObject client = (JSONObject) result[0];
-    JSONObject account = (JSONObject) client.get(accountName);
+    client = (JsonObject) result[0];
+    account = (JsonObject) client.get(accountName);
 
-    return new Object[]{account, result[1]};
+    return new Object[] { account, result[1] };
   }
 
-  //public static void deposit(int quantity, String accountName, String cardNumber) {
-    //long balance = getBalance(accountName, cardNumber);
-    //balance += quantity;
-  //}
+  public static void deposit(int quantity, String accountName, String cardNumber) {
+    Object[] result = getAccount(accountName, cardNumber);
+
+    if (result != null) {
+      JsonObject db = (JsonObject) result[1];
+      JsonObject account;
+      JsonKey balanceKey = Jsoner.mintJsonKey("balance", DEFAULT_BALANCE);
+      int balance;
+      Writer writer;
+
+      account = (JsonObject) result[0];
+      balance = account.getInteger(balanceKey) + quantity;
+
+      account.put("balance", balance);
+
+      // write to output file
+      try {
+        writer = new FileWriter(DB_PATH);
+      } catch (IOException e) {
+        e.printStackTrace();
+        return;
+      }
+
+      try {
+        writer.write(Jsoner.prettyPrint(db.toJson()));
+      } catch (IOException e) {
+        e.printStackTrace();
+
+        try {
+          writer.close();
+        } catch (IOException e1) {
+          e1.printStackTrace();
+        }
+
+        return;
+      }
+
+      try {
+        writer.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
